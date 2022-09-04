366923df423df4b4a3622362b1758ac36f36fa79
Андрій Мельничук <Andrii.Melnychuk@cs.khpi.edu.ua>
2022-09-04 12:34:56 +0200

    04.09.2022/0A,0B

--- №0B/№0B/№0B.cpp
+++ №0B/№0B/№0B.cpp
@@ -2,10 +2,102 @@
 //
 
 #include <iostream>
+using namespace std;
 
+void pointers(int* x, int* y, int* z)
+{
+    float min, max,a,b,c;
+
+    min = *x;
+    
+        if (min > *y)
+            min = *y;
+     
+        if (min > *z)
+            min = *z;
+    
+
+    max = *x;
+    
+        if (max < *y)
+            max = *y;
+        
+        if (max < *z)
+            max = *z;
+        a = *x;
+        b = *y;
+        c = *z;
+    
+    if (min == *x)
+       *x = max; 
+    else  if (min == *y)
+       *y = max;
+    else  if (min == *z)
+       *z = max;
+
+    if (max == a)
+        *x = min;
+    else  if (max == b)
+        *y = min;
+    else  if (max == c)
+        *z = min;
+     
+}
+void link(int& x, int& y, int& z)
+{
+    float min, max, a, b, c;
+
+    min = x;
+
+    if (min > y)
+        min = y;
+
+    if (min > z)
+        min = z;
+
+
+    max = x;
+
+    if (max < y)
+        max = y;
+
+    if (max < z)
+        max = z;
+    a = x;
+    b = y;
+    c = z;
+
+    if (min == x)
+        x = max;
+    else  if (min == y)
+        y = max;
+    else  if (min == z)
+        z = max;
+
+    if (max == a)
+        x = min;
+    else  if (max == b)
+        y = min;
+    else  if (max == c)
+        z = min;
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int x, y, z;
+    cin >> x >> y >> z;
+
+    cout << x << " " << y  << " " << z << endl;
+    pointers(&x, &y, &z);
+    cout << x << " " << y << " " << z << endl;
+
+
+    cin >> x >> y >> z;
+
+    cout << x << " " << y << " " << z << endl;
+    link(x, y, z); 
+    cout << x << " " << y << " " << z << endl;
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
