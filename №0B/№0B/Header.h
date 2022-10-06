0218efdf3e363485a902481a63d5f71ec7c072b9
Андрій Мельничук <Andrii.Melnychuk@cs.khpi.edu.ua>
2022-10-06 22:15:37 +0200

    pered

--- №0B/№0B/Header.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-void pointers(int* x, int* y, int* z);
-void link(int& x, int& y, int& z);
--- №0B/№0B/№0B.cpp
+++ №0B/№0B/№0B.cpp
@@ -1,25 +1,125 @@
 ﻿// №0B.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+class replaceValues
+{
+public:
+	void link(int& x, int& y, int& z)
+	{
+		float min, max, a, b, c;
+
+		min = x;
+
+		if (min > y)
+			min = y;
+
+		if (min > z)
+			min = z;
 
 
-#include "Header.h"
+		max = x;
+
+		if (max < y)
+			max = y;
+
+		if (max < z)
+			max = z;
+
+
+		a = x;
+		b = y;
+		c = z;
+
+		if (min == x)
+			x = max;
+		else  if (min == y)
+			y = max;
+		else  if (min == z)
+			z = max;
+
+		if (max == a)
+			x = min;
+		else  if (max == b)
+			y = min;
+		else  if (max == c)
+			z = min;
+
+	}
+
+	void pointers(int* x, int* y, int* z)
+	{
+		float min, max, a, b, c;
+
+		min = *x;
+
+		if (min > *y)
+			min = *y;
+
+		if (min > *z)
+			min = *z;
+
+
+		max = *x;
+
+		if (max < *y)
+			max = *y;
+
+		if (max < *z)
+			max = *z;
+
+
+		a = *x;
+		b = *y;
+		c = *z;
+
+		if (min == *x)
+			*x = max;
+		else  if (min == *y)
+			*y = max;
+		else  if (min == *z)
+			*z = max;
+
+		if (max == a)
+			*x = min;
+		else  if (max == b)
+			*y = min;
+		else  if (max == c)
+			*z = min;
+
+	}
+};
 int main()
 {
-    int x, y, z;
-    cin >> x >> y >> z;
-
-  
-    pointers(&x, &y, &z);
-    cout << x << " " << y << " " << z << endl;
-
-
-    cin >> x >> y >> z;
-
-
-    link(x, y, z); 
-    cout << x << " " << y << " " << z << endl;
-    
+	replaceValues value;
+	int x, y, z;
+	do
+	{
+		system("cls");
+		cout << "Enter x, y, z\n";
+		cin >> x >> y >> z;
+		if (cin.fail()) {
+			cout << "Eror, try one more time\n";
+			cin.clear();
+			cin.ignore(32767, '\n');
+			system("pause");
+			system("cls");
+			continue;
+		}
+		system("cls");
+		cout << "Before swap:\nx = " << x << " y = " << y << " z = " << z << endl;
+		value.pointers(&x, &y, &z);
+		cout << "After swap by link:\nx = " << x << " y = " << y << " z = " << z << endl;
+		cout << "Before swap:\nx = " << x << " y = " << y << " z = " << z << endl;
+		value.link(x, y, z);
+		cout << "After swap by pointers:\nx = " << x << " y = " << y << " z = " << z << endl;
+		cout << "\n";
+		cout << "Press any key to continue . . .\n";
+		puts("Press 'esc' to exit . . .");
+	} while (_getch() != 27);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
