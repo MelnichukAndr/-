da275a25d9e1d7cdfa98afd5cc9ee8ad64dcbf04
Андрій Мельничук <Andrii.Melnychuk@cs.khpi.edu.ua>
2022-10-06 22:01:14 +0200

    pered

--- №0A/№0A/Header.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-using namespace std;
-#define _CRT_SECURE_NO_WARNINGS
-void square(int a, int b = 0);
--- №0A/№0A/№0A.cpp
+++ №0A/№0A/№0A.cpp
@@ -1,27 +1,74 @@
 ﻿// №0A.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include "Header.h"
+#include <iostream>
+using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+
+class figure {
+public:
+    int Inc(int a = 2, int b = 0)
+    {
+        if (a == 0)
+            return b * b;
+        if (b == 0)
+            return a * a;
+        return a * b;
+    }
+};
 
 
 int main()
 {
-    int s=0, r=0;
-
-    cout << "Enter the side of the square" << endl;
-    cout << "If you want to calculate the area of a rectangle enter 0" << endl;
-    cin >> s;
-    if (s > 0)
-        square(s);
-    if (s == 0)
-    {
-        cout << "Enter the sides of the rectangle" << endl;
-        cin >> s >> r;
-        square(s,r);
-    }
-    else if (s < 0 || r < 0)
-        cout << "Error:" << "Side cannot be equal to or less than 0";
     
-  
+    figure area;
+    int a;
+    do {
+        cout << "Square - 1\nRectangle - 2\n";
+
+        cin >> a;
+        if (a == 1) {
+            cout << "Enter length side >";
+            int b;
+            cin >> b;
+            if (b > 0)
+                cout << "Figure area = " << area.Inc(b) << "\n\n\n";
+            else {
+
+                cout << "Eror, try one more time\n";
+                cin.clear();
+                cin.ignore(32767, '\n');
+                system("pause");
+                system("cls");
+                continue;
+            }
+        }
+        else if (a == 2) {
+            cout << "Enter length a,b sides >";
+            int n, m;
+            cin >> n >> m;
+            if (n > 0 && m > 0) {
+                cout << "Figure area = " << area.Inc(n, m) << "\n\n\n";
+            }
+            else {
+                cout << "Eror, try one more time\n";
+                cin.clear();
+                cin.ignore(32767, '\n');
+                system("pause");
+                system("cls");
+                continue;
+            }
+        }
+        else {
+            cout << "Eror\n";
+            cin.clear();
+            cin.ignore(32767, '\n');
+            system("pause");
+            system("cls");
+            continue;
+        }
+        cout << "Press any key to continue . . .\n";
+        puts("Press 'esc' to exit . . .");
+    } while (_getch() != 27);
 }
 
